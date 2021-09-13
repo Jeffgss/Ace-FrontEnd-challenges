3f0c972ea2dc39378e2052f7a0ecce74604530a1
Jeff <jefferson.santos@maxmilhas.com.br>
2021-09-13 10:55:49 -0300

    refactor critter challenge

--- critter/index.js
+++ critter/index.js
@@ -1,41 +1,38 @@
-const api = "https://www.acefrontend.com/c/critter/feed.json";
 const mainCreets = document.querySelector(".creets");
 
-function init() {
-  fetch(api)
-    .then((response) => {
-      response.json().then((data) => {
-        feed = data.feed;
+const loadData = async () => {
+  try {
+    const url = "https://www.acefrontend.com/c/critter/feed.json";
+    const response = await fetch(url);
+    const data = await response.json();
+    const feed = data.feed;
+    return feed;
+  } catch (err) {
+    console.error(err);
+  }
+};
 
-        function criaItem(feed) {
-          const card = document.createElement("div");
-          card.classList.add("card");
+const createCard = (post) => {
+  const card = document.createElement("div");
+  card.classList.add("card");
 
-          card.innerHTML = `
-            <img src=${feed.user.avatar}/>
-            <div class="userContent">
-              <h2>@${feed.user.username} <span>· ${feed.created_at.slice(
-            4,
-            10
-          )}</span> </h2>
-              <p> ${feed.text} </p>
-              <span> Likes: ${feed.likes} </span>
-            </div>
-            `;
-          mainCreets.appendChild(card);
-        }
+  card.innerHTML = `
+    <img src=${post.user.avatar}/>
+    <div class="userContent">
+    <h2>@${post.user.username} <span>· ${post.created_at.slice(
+    4,
+    10
+  )}</span> </h2>
+    <p> ${post.text} </p>
+    <span> Likes: ${post.likes} </span>
+    </div>
+  `;
+  mainCreets.appendChild(card);
+};
 
-        feed.forEach((item) => {
-          criaItem(item);
-        });
-      });
-    })
-    .catch((error) => {
-      console.error(error);
-      mainCreets.innerHTML = `
-      <span class="errorMessage">${error.message}</span>
-      `;
-    });
-}
-
-init();
+(async () => {
+  const feed = await loadData();
+  feed.forEach((post) => {
+    createCard(post);
+  });
+})();
